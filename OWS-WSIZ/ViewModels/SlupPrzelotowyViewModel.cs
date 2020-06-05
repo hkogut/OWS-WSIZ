d71c0ef13887268c6a6f8c63eecadea53082a388
Beata Dudek <bebe.dudek@gmail.com>
2020-06-05 16:10:13 +0200

    Add images and user control - SlupPrzelotowyView

---
+++ OWS-WSIZ/ViewModels/SlupPrzelotowyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OWS_WSIZ.ViewModels
+{
+    public class SlupPrzelotowyViewModel
+    {
+        
+    }
+}
